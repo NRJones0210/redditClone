5428b7f4e371d5b9c620e3a9c35adb48353b9c73
Nick Jones <NRJones0210@gmail.com>
2015-11-30 18:32:44 -0700

    added toggle to show/hide post form

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,3 +1,7 @@
 app.controller('redditController', function($scope) {
 
+  $scope.showPostForm = function() {
+    $scope.newPostForm.visable = ($scope.newPostForm.visable === true) ? false : true
+  }
+  
 })
