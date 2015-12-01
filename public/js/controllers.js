403c084353425848a332ae6c2ca18f715a47991b
Nick Jones <NRJones0210@gmail.com>
2015-11-30 18:40:49 -0700

    added function to submit new post into posts array

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,5 +3,22 @@ app.controller('redditController', function($scope) {
   $scope.showPostForm = function() {
     $scope.newPostForm.visable = ($scope.newPostForm.visable === true) ? false : true
   }
+
+  $scope.posts = [];
+
+  $scope.addPost = function() {
+    $scope.posts.push({
+      title: $scope.newPost.title,
+      author: $scope.newPost.author,
+      image: $scope.newPost.image,
+      description: $scope.newPost.description,
+      voteCount: 0,
+      date: Date.now(),
+      comments: []
+    })
+    $scope.newPost = {};
+    $scope.newPostForm.visible = false;
+    console.log($scope.posts)
+  };
   
 })
